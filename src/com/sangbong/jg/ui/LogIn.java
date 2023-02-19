f25d77a885ae98fb3fdb4e8e9f78f805a3f96dad
filson1205 <sondongfil1205@gmail.com>
2023-02-20 08:41:47 +0900

    [feat ]로그인화면

---
+++ src/com/sangbong/jg/ui/LogIn.java
@@ -0,0 +1,71 @@
+package com.sangbong.jg.ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class LogIn extends JFrame {
+	
+	public LogIn() {
+		
+		JPanel panel = new JPanel();
+		
+		JLabel label = new JLabel("이메일");
+		label.setBounds(55, 122, 53, 15);
+		JLabel pswrd = new JLabel("비밀번호");
+		pswrd.setBounds(55, 183, 69, 15);
+		JTextField txtID = new JTextField(10);
+		txtID.setText("이메일");
+		txtID.setToolTipText("");
+		txtID.setBounds(65, 145, 452, 21);
+		JPasswordField txtPass = new JPasswordField(10);
+		txtPass.setToolTipText("");
+		txtPass.setBounds(65, 208, 452, 21);
+		JButton logBtn = new JButton("로그인");
+		logBtn.setBounds(0, 263, 584, 39);
+		panel.setLayout(null);
+		
+		panel.add(label);
+		panel.add(txtID);
+		panel.add(pswrd);
+		panel.add(txtPass);
+		panel.add(logBtn);
+	
+		
+		
+		logBtn.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		getContentPane().add(panel);
+		
+		JButton btnNewButton = new JButton("회원가입");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(0, 312, 584, 39);
+		panel.add(btnNewButton);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 0, 584, 112);
+		panel.add(panel_1);
+		
+		setSize(600,400);
+		
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		new LogIn();
+	}
+}
