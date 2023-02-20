6f13737326db5c467b39d63d50d8782060419939
filson1205 <sondongfil1205@gmail.com>
2023-02-20 18:30:30 +0900

    [feature] 게시글 삭제

---
+++ src/com/sangbong/jg/ui/PostDelete.java
@@ -0,0 +1,75 @@
+package com.sangbong.jg.ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Font;
+
+/**
+ * <pre>
+ * Class : PostDelete
+ * Comment : 게시글 삭제 화면 구현
+ * History
+ * 2023/02/20 (손동필) 처음 작성함
+ * </pre>
+ * @author 손동필
+ * @version 1.0.0
+ * @see 
+ * */
+public class PostDelete extends JFrame {
+	private JLabel lblNewLabel = new JLabel("Logo");
+	
+	public PostDelete() {
+		
+		setSize(1280, 720);
+		
+		JPanel panel = new JPanel();
+		JButton logBtn = new JButton("취소");
+		logBtn.setBounds(351, 570, 300, 50);
+		panel.setLayout(null);
+		panel.add(logBtn);
+	
+		
+		
+		logBtn.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		getContentPane().add(panel);
+		
+		JButton btnNewButton = new JButton("확인");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(663, 570, 300, 50);
+		panel.add(btnNewButton);
+		lblNewLabel.setBackground(new Color(0, 0, 0));
+		lblNewLabel.setBounds(340, 0, 600, 150);
+		panel.add(lblNewLabel);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JLabel lblNewLabel_1 = new JLabel("게시글 삭제를 진행하시겠습니까?");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("굴림", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(490, 307, 300, 30);
+		panel.add(lblNewLabel_1);
+		
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		new PostDelete();
+	}
+}
