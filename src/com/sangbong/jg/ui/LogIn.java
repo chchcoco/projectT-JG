ea814f11d041a29c02db88b58682ccd35bda5794
filson1205 <sondongfil1205@gmail.com>
2023-02-20 18:29:57 +0900

    [feature] 로그인 수정

--- src/com/sangbong/jg/ui/LogIn.java
+++ src/com/sangbong/jg/ui/LogIn.java
@@ -10,27 +10,42 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import java.awt.Color;
 
+/**
+ * <pre>
+ * Class : Login
+ * Comment : 로그인 화면 구현
+ * History
+ * 2023/02/19 (손동필) 처음 작성함
+ * </pre>
+ * @author 손동필
+ * @version 1.0.0
+ * @see 
+ * */
 public class LogIn extends JFrame {
-	private final JLabel lblNewLabel = new JLabel("Logo");
+	private JLabel lblNewLabel = new JLabel("Logo");
 	
 	public LogIn() {
 		
+		setSize(1280, 720);
+		
 		JPanel panel = new JPanel();
+//		panel.setSize(1280, 720);
 		
 		JLabel label = new JLabel("이메일");
-		label.setBounds(55, 122, 53, 15);
+		label.setBounds(390, 229, 53, 15);
 		JLabel pswrd = new JLabel("비밀번호");
-		pswrd.setBounds(55, 183, 69, 15);
+		pswrd.setBounds(390, 322, 69, 15);
 		JTextField txtID = new JTextField(10);
 		txtID.setText("이메일");
 		txtID.setToolTipText("");
-		txtID.setBounds(65, 145, 452, 21);
+		txtID.setBounds(390, 254, 500, 40);
 		JPasswordField txtPass = new JPasswordField(10);
 		txtPass.setToolTipText("");
-		txtPass.setBounds(65, 208, 452, 21);
+		txtPass.setBounds(390, 347, 500, 40);
 		JButton logBtn = new JButton("로그인");
-		logBtn.setBounds(0, 263, 584, 39);
+		logBtn.setBounds(340, 505, 600, 50);
 		panel.setLayout(null);
 		
 		panel.add(label);
@@ -54,18 +69,12 @@ public class LogIn extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(0, 312, 584, 39);
+		btnNewButton.setBounds(340, 574, 600, 50);
 		panel.add(btnNewButton);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 0, 584, 112);
-		panel.add(panel_1);
-		panel_1.setLayout(null);
+		lblNewLabel.setBackground(new Color(0, 0, 0));
+		lblNewLabel.setBounds(340, 0, 600, 150);
+		panel.add(lblNewLabel);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(0, 0, 584, 112);
-		panel_1.add(lblNewLabel);
-		
-		setSize(600,400);
 		
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
